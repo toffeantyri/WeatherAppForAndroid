d5b4a754f18d3c60d7bdbfc38437601f99462f50
toffeantyri <tofy3434@yandex.ru>
2022-03-05 15:27:16 +0600

    Верстка экрана, BottomSheets SwipePanel добавлена.
    Внимание
    реализован BottomSheets
    в стиле изменена строка состояния

--- app/src/main/java/ru/weather/weathertoday/MainActivity.kt
+++ app/src/main/java/ru/weather/weathertoday/MainActivity.kt
@@ -2,11 +2,31 @@ package ru.weather.weathertoday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_main_city.text = "Moskow"
+        tv_main_date.text = "01.01.2022"
+
+        iv_weather_status.setImageDrawable(getDrawable(R.drawable.ic_oblako_24))
+        tv_status_weather.text = "Облачно"
+        tv_current_temperature.text = "25"+"\u00b0"
+        min_temp_value.text = "19"
+        max_temp_value.text = "32"
+
+        iv_weather_image.setImageResource(R.mipmap.unionx4)
+        davlenie_status.text = "15 mPa"
+        vlazhnost_status.text = "50%"
+        skorost_vetra_status.text = "15m/s"
+        sunrise_status.text = "6:00"
+        sunset_status.text = "20:00"
+
+
+
     }
 }

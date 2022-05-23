171bce12b2fce71deae08a8efefba95dd7043350
toffeantyri <tofy3434@yandex.ru>
2022-05-23 11:39:50 +0600

    Добавлена Активити для SettingActivity Layout Setting is ready

---
+++ app/src/main/java/ru/weather/weathertoday/activity/SettingActivity.kt
@@ -0,0 +1,12 @@
+package ru.weather.weathertoday.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
